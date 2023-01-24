366cc8c02242f22840c6697b7ae0715a3327719c
kartikeya <kartikeyasharma2006@gmail.com>
2023-01-24 18:50:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import { Route,Routes,useNavigate } from 'react-router-dom'
+import Login from './components/Login'
+import Home from './container/Home'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+        <Routes>
+          <Route path='login' element={<Login/>}/>
+          <Route path='/*' element={<Home/>}/>
+        </Routes>
+    </GoogleOAuthProvider>
+
+  )
 }
 
-export default App;
+export default App
